e248e7b62e1432b4786387a1ebf353866b1f313b
Vivian <vivianfigueredo17@gmail.com>
2023-09-24 12:24:12 -0300

    agregando footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router,Routes, Route} from "react-router-dom";
 import AppRoutes from "./routes/Routes"
 import NavbarBs from './components/Navbar/Navbar';
-import {Footer} from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import 'react-bootstrap';
 
 function App() {
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,76 +1,72 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
 
-export const Footer = () => {
-  return (
-    <>
-    <footer class="footer">
-    <div class="footer-container">
-
-        {/* <div class="col-footer">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105227.9233705569!2d-58.99930865524429!3d-34.493436907091194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9cb6643d2003%3A0xa8da0402f634b636!2sPilar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1689175001579!5m2!1ses!2sar"
-                width="280" height="250" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div> */}
-
-        <div class="col-footer">
-            <h5>Nosotros</h5>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem reprehenderit illum
-                aliquam veniam cum, deleniti vero unde optio minus maxime accusamus obcaecati cumque
-                corporis soluta! Aliquid similique perferendis impedit quia.</p>
+const Footer = () => {
+  return (    
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="col-footer">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105227.9233705569!2d-58.99930865524429!3d-34.493436907091194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9cb6643d2003%3A0xa8da0402f634b636!2sPilar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1689175001579!5m2!1ses!2sar"
+            title="Mapa de ubicación de MinimalHome" width="280" height="250" style={{ border: "0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
 
-        <div class="col-footer">
-            <h5>Navegación</h5>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Productos</a></li>
-                <li><a href="#">Nosotros</a></li>
-                <li><a href="#">Ayuda</a></li>
-            </ul>
+        <div className="col-footer">
+          <h5>Nosotros</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem reprehenderit illum
+            aliquam veniam cum, deleniti vero unde optio minus maxime accusamus obcaecati cumque
+            corporis soluta! Aliquid similique perferendis impedit quia.</p>
         </div>
 
-        <div class="col-footer">
-            <h5>Contacto</h5>
-            <p><i class="bi bi-envelope-fill"></i> MinimalHome@gmail.com</p>
-            <p><i class="bi bi-telephone-fill"></i> +54 230-43788743</p>
-            <p><i class="bi bi-chat-right-text"></i> Ayuda en línea</p>
+        <div className="col-footer">
+          <h5>Navegación</h5>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Ayuda</a></li>
+          </ul>
         </div>
 
-    </div>
+        <div className="col-footer">
+          <h5>Contacto</h5>
+          <p><i className="bi bi-envelope-fill"></i> MinimalHome@gmail.com</p>
+          <p><i className="bi bi-telephone-fill"></i> +54 230-43788743</p>
+          <p><i className="bi bi-chat-right-text"></i> Ayuda en línea</p>
+        </div>
+      </div>
 
-    {/* </div>
-    <hr class="mb-4">
-    <div class="text-center mb-2">
+      <hr className="mb-4" />
+
+      <div className="text-center mb-2">
         <p>&copy; Todos los derechos reservados <strong>MinimalHome</strong></p>
-    </div> */}
+      </div>
 
-    <div class="text-center">
-        <ul class="list-unstyled list-inline">
-            <li class="list-inline-item">
-                <a href="#" class="text-white"> <i class="bi bi-linkedin"></i> </a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#" class="text-white"> <i class="bi bi-youtube"></i> </a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#" class="text-white"> <i class="bi bi-facebook"></i> </a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#" class="text-white"> <i class="bi bi-discord"></i></a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#" class="text-white"> <i class="bi bi-github"></i></a>
-            </li>
-            <li class="list-inline-item">
-                <a href="#" class="text-white"> <i class="bi bi-google"></i></a>
-            </li>
+      <div className="text-center">
+        <ul className="list-unstyled list-inline">
+          <li className="list-inline-item">
+            <a href="#" className="text-white"> <i className="bi bi-linkedin"></i> </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white"> <i className="bi bi-youtube"></i> </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white"> <i className="bi bi-facebook"></i> </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white"> <i className="bi bi-discord"></i></a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white"> <i className="bi bi-github"></i></a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white"> <i className="bi bi-google"></i></a>
+          </li>
         </ul>
+      </div>
+    </footer>
+  );
+};
 
-    </div>
-
-</footer>
-
-</>
-  )
-                                }
+export default Footer;
