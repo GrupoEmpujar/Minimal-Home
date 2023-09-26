a557b7f01d9dbfb42f0afd58371d58d5690de2b8
Lucas Colque <lucascolque35@gmail.com>
2023-09-25 21:45:02 -0300

    Por hoy martes 25/9 ya estan los cambios, mañana ver retoques

--- src/components/DetalleItem/DetalleItem.jsx
+++ src/components/DetalleItem/DetalleItem.jsx
@@ -19,6 +19,7 @@ const DetalleItem = () => {
     }
     useEffect(() => {
         peticiones();
+            window.scrollTo(0, 0);
     }, [])
     const handleSuma = () =>{
         if(count >= item.stock){
